42500e233aec3a67faf968ed736ddb4f920e32e4
kasa <154661151+kasa538@users.noreply.github.com>
2024-07-16 23:26:40 +0900

    p146 バリデータを作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,17 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Nette\Utils\Validators;
+use PMA\libraries\config\Validator as ConfigValidator;
+use Validator;
 
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index',['msg'=>'フォームを入力：']);
+        return view('hello.index', ['msg' => 'フォームを入力：']);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        return view('hello.index',['msg'=>'正しく入力されました！']);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
+        return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
